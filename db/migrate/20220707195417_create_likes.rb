1a377f755d23ade9da065bdb1a98e3f84600def6
CRClothier <c.r.clothier@gmail.com>
2022-07-07 18:57:08 -0500

    Generate schema from migrations

--- db/migrate/20220707195417_create_likes.rb
+++ db/migrate/20220707195417_create_likes.rb
@@ -1,7 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-
+      t.belongs_to :user, index: true, foreign_key: true
+      t.belongs_to :post, index: true, foreign_key: true
       t.timestamps
     end
   end
