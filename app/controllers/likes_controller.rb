2d4a942c5d0739c04d418e9debc51ff7f637faae
Chris Clothier <c.r.clothier@gmail.com>
2022-07-16 12:10:09 -0500

    Check resourse has been saved

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,7 +3,11 @@ class LikesController < ApplicationController
     @user = current_user
     @post = Post.find(params[:post_id])
     @like = Like.new(user: @user, post: @post)
-    @like.save
-    redirect_to user_post_path(@user, @post)
+    if @like.save
+      format.html { redirect_to user_post_path(@user, @post) }
+    else
+      format.html { redirect_to @post }
+    end
+    
   end
 end
